466eeed65e84809b28a863952d59d7cc86e6b726
Andrew Foote <andrew.foote@roar-software.com>
2019-12-03 17:21:13 +0000

    it's not working! :(

--- 3.py
+++ 3.py
@@ -1,32 +1,55 @@
-with open('inputs/3.txt') as f:
-    wire1, wire2 = (line.split(',') for line in f)
+from collections import defaultdict
+from dataclasses import dataclass
+import itertools as it
+
+@dataclass
+class Vector:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __abs__(self):
+        return abs(self.x) + abs(self.y)
 
 unit_vectors = {
-    'R': [1, 0],
-    'D': [0, -1],
-    'L': [-1, 0],
-    'U': [1, 0],
+    'R': Vector(1, 0),
+    'D': Vector(0, -1),
+    'L': Vector(-1, 0),
+    'U': Vector(1, 0),
 }
 
+def parse_wire(src):
+    return [(unit_vectors[token[0]], int(token[1:])) for token in src.split(',')]
+
 def path(wire):
-    pos = [0, 0]
+    pos = Vector(0, 0)
 
-    for vec_src in wire:
-        direction = vec_src[0]
-        magnitude = int(vec_src[1:])
-        vec = unit_vectors[direction]
-        
+    for direction, magnitude in wire:        
         for _ in range(magnitude):
-            pos[0] += vec[0]
-            pos[1] += vec[1]
-            yield tuple(pos)
+            pos += direction
+            yield pos
 
-def find_intersection_point(wire1, wire2):
-    path1 = set(path(wire1))
+def first_intersection(wire1, wire2):
+    path1 = path(wire1)
     path2 = path(wire2)
+    visitors = defaultdict(lambda: set())
 
-    for pos in path2:
-        if pos in path1:
-            return abs(pos[0]) + abs(pos[1])
+    for pos1, pos2 in it.zip_longest(path1, path2):
+        visitors[pos1].add(1)
+        visitors[pos2].add(2)
 
-print(find_intersection_point(wire1, wire2))
+        if len(visitors[pos1]) == 2:
+            return abs(pos1)
+
+        if len(visitors[pos2]) == 2:
+            return abs(pos1)
+
+with open('inputs/3.txt') as f:
+    wire1, wire2 = map(parse_wire, f)
+
+print(first_intersection(wire1, wire2))
