db98f10f64881eb0f375124f256bfd1860d099aa
Andrew Foote <footeandrew1@gmail.com>
2019-12-04 19:44:50 +0000

    refactor 3 a bit

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True)
 class Vector:
     x: int
     y: int
@@ -8,18 +8,24 @@ class Vector:
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __hash__(self):
-        return hash((self.x, self.y))
+    def __rmul__(self, scalar):
+        return Vector(scalar * self.x, scalar * self.y)
+
+    def __truediv__(self, scalar):
+        return (1 / scalar) * self
 
     def __abs__(self):
         return abs(self.x) + abs(self.y)
 
+    def unit(self):
+        return self / abs(self)
+
 def path(wire):
     pos = Vector(0, 0)
 
-    for direction, magnitude in wire:        
-        for _ in range(magnitude):
-            pos += direction
+    for vector in wire:        
+        for _ in range(abs(vector)):
+            pos += vector.unit()
             yield pos
 
 unit_vectors = {
@@ -29,8 +35,8 @@ unit_vectors = {
     'U': Vector(0, 1),
 }
 
-def parse_wire(src):
-    return [(unit_vectors[token[0]], int(token[1:])) for token in src.split(',')]
+def parse_wire(wire):
+    return [int(token[1:]) * unit_vectors[token[0]] for token in wire.split(',')]
 
 with open('inputs/3.txt') as f:
     wire1, wire2 = map(parse_wire, f)
